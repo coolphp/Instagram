7af9b526d81c65a689c2265442d053277f4714e4
Igor <khigor777@yandex.ru>
2016-01-04 08:46:02 +0300

    #6 Del pass

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/insta'
 
 SMTP_USER = 'khigor777@mail.ru'
-SMTP_PASSWORD = 'hfcrhenrfltkf'
+
+SMTP_PASSWORD = ''
 SMTP_SERVER = 'smtp.mail.ru'
 SMTP_MSG = u'Подтвердите регистрацию на сайте FUNINSTA.RU перейдя по ссылке: '
 SMTP_NOTIFICATION_TO = 'khigor777@yandex.ru'
